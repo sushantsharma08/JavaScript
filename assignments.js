0611bbedfe3368f5e3fd532df195eae85d66d944
Sushant Sharma <ssushantlb08@gmail.com>
2021-08-17 00:09:00 +0530

    doc:assignment-data types

---
+++ assignments.js
@@ -0,0 +1,17 @@
+//values and variables
+let COUNTRY = "INDIA";
+let CONTINENT = "ASIA";
+let POPULATION = "1378.6";
+console.log(COUNTRY);
+console.log(CONTINENT);
+console.log(POPULATION);
+//data types
+let isISLAND ="peninsula";
+console.log(isISLAND);
+console.log(typeof isISLAND);
+ 
+isISLAND =true;
+console.log("new data type value:")
+console.log(typeof isISLAND);
+
+let language = "";
--- first assignment.js
+++
@@ -1,6 +0,0 @@
-let COUNTRY = "INDIA";
-let CONTINENT = "ASIA";
-let POPULATION = "1378.6";
-console.log(COUNTRY);
-console.log(CONTINENT);
-console.log(POPULATION);
