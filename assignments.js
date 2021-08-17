f922aff0c518d24293fb96f308a274356a1b9ee0
Sushant Sharma <ssushantlb08@gmail.com>
2021-08-17 16:08:03 +0530

    doc: basic operations

--- assignments.js
+++ assignments.js
@@ -13,5 +13,24 @@ console.log(typeof isISLAND);
 isISLAND =true;
 console.log("new data type value:")
 console.log(typeof isISLAND);
-
-let language = "";
+//let const var
+let language = "hindi";
+console.log(language);
+ const pin = 91 ;
+ console.log(pin);
+ //pin = 0
+//basic operations
+ console.log("half population:"+" "+POPULATION/2);
+ POPULATION ++;//inc population by 1 or can use pop +=1 similarly can use -= *= /=
+ console.log("population plus one:"+POPULATION);
+ let PopulationFinland = "33"
+ console.log("population of finaland:"+PopulationFinland);
+ //comPARISION OPERATOR
+ console.log("my country's population more than findland's population");
+console.log("my country's population more than findland's population"+POPULATION > PopulationFinland);
+let AvgPopulation = 33;
+console.log("my country's population is less than avg population of country");
+console.log(POPULATION<AvgPopulation);
+let Description ='Portugal is in Europe, and its 11 million people speak portuguese';
+console.log(Description);
+//
