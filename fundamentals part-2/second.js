685d245b383d2f2bf32fd027bd1e4ca8da7912bb
Sushant Sharma <ssushantlb08@gmail.com>
2021-08-23 23:39:41 +0530

    doc:function

---
+++ fundamentals part-2/second.js
@@ -0,0 +1,17 @@
+'use strict';
+
+//functions
+function logg() {
+ console.log(`hi i am sushant`);   
+}
+//calling function
+logg();
+logg();
+
+//example
+function fruitprocessor(apples,oranges) {
+    console.log(apples,oranges);
+    const juice = `juice with ${apples} apples and ${oranges} oranges.`;
+    return 0;
+}
+fruitprocessor(5,0)
