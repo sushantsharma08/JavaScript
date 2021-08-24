ca6b6755cbd2fd5dca1bc373c946f8704f14132c
Sushant Sharma <ssushantlb08@gmail.com>
2021-08-24 22:05:50 +0530

    feature: function declaration vs expression

--- fundamentals part-2/second.js
+++ fundamentals part-2/second.js
@@ -6,7 +6,7 @@ function logg() {
 }
 //calling function
 logg();
-logg();
+
 
 //example
 function fruitprocessor(apples,oranges) {
@@ -16,6 +16,22 @@ function fruitprocessor(apples,oranges) {
 }
 const applejuice = fruitprocessor(5,0);
 console.log(applejuice);
-
 const appleorangejuice = fruitprocessor(2,2);
 console.log(appleorangejuice);
+
+//function declarations vs function expressions
+//we will take an exapmle of age 
+
+// 1. function declaration
+function calcAge1(birthYear) {
+    return 2021-birthYear;
+}
+const age1 = calcAge1(2002);
+console.log(age1);
+
+// 2. function expression
+const calcage2 = function (birthYear) {
+    return 2021-birthYear
+}
+const age2 = calcage2(2001);
+console.log(age2);
