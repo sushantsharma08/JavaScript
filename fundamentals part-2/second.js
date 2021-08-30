648c65d3221897d83265b4a8dc1f05ec08e720b8
Sushant Sharma <ssushantlb08@gmail.com>
2021-08-30 20:03:22 +0530

    minor fix

--- fundamentals part-2/second.js
+++ fundamentals part-2/second.js
@@ -2,7 +2,7 @@
 
 //functions
 function logg() {
- console.log(`hi i am sushant`);   
+    console.log(`hi i am sushant`);
 }
 //calling function
 logg();
@@ -18,44 +18,43 @@ const applejuice = fruitprocessor(5,0);
 console.log(applejuice);
 const appleorangejuice = fruitprocessor(2,2);
 console.log(appleorangejuice);*/ //commented coz going to use same function for function calling function (line 50)
- 
+
 //function declarations vs function expressions
 //we will take an exapmle of age 
 
 // 1. function declaration
 function calcAge1(birthYear) {
-    return 2021-birthYear;
+    return 2021 - birthYear;
 }
 const age1 = calcAge1(2002);
 // console.log(age1);
 
 // 2. function expression 
 const calcage2 = function (birthYear) {
-    return 2021-birthYear
+    return 2021 - birthYear
 }
 const age2 = calcage2(2001);
 // console.log(age2);
 
-console.log(age1,age2);
+console.log(age1, age2);
 
 //arrow function 
-const yearsUntilRetirement = (birthYear,Firstname) =>
-{
-const age = 2021-birthYear;
-const retirement = 60-age;
-return `${Firstname} retires in ${retirement} years`;
+const yearsUntilRetirement = (birthYear, Firstname) => {
+    const age = 2021 - birthYear;
+    const retirement = 60 - age;
+    return `${Firstname} retires in ${retirement} years`;
 }
-console.log(yearsUntilRetirement(2002,'sushant'));
+console.log(yearsUntilRetirement(2002, 'sushant'));
 
 //function calling function 
 //first function
 function cutFruitPieces(fruit) {
-    return fruit*4 // as the fruit needs to be cut into 4 peices before fruitprocessor
+    return fruit * 4 // as the fruit needs to be cut into 4 peices before fruitprocessor
 }
 //second function
-function fruitprocessor(apples,oranges) {
+function fruitprocessor(apples, oranges) {
     const applepieces = cutFruitPieces(apples);
-    const orangepieces = cutFruitPieces(oranges); 
+    const orangepieces = cutFruitPieces(oranges);
     return `juice with ${applepieces} pieces of apples and ${orangepieces} pieces of oranges`
 }
-console.log(fruitprocessor(2,3)); 
+console.log(fruitprocessor(2, 3));
