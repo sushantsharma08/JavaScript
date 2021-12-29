df8fb2fd5459a693646311824d41698e04d6ef7f
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2021-12-29 11:30:28 +0530

    click event listener

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,8 +1,15 @@
-'use strict';
- console.log(document.querySelector('.message').textContent);
+// 'use strict';
+//  console.log(document.querySelector('.message').textContent);
 
- document.querySelector('.message').textContent = 'yoo boi wassup';
+//  document.querySelector('.message').textContent = 'yoo boi wassup';
 
- document.querySelector('.number').textContent = 4;
- document.querySelector('.score').textContent = 30;
- document.querySelector('.guess').value = 7;
+//  document.querySelector('.number').textContent = 4;
+//  document.querySelector('.score').textContent = 30;
+//  document.querySelector('.guess').value = 7;
+
+
+// adding a click event
+
+document.querySelector('.check').addEventListener('click' , function () {
+    console.log(document.querySelector('.guess').value);
+});
