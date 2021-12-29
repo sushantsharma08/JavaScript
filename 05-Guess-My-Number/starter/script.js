8e9e1069cc7011fed6d88c9bb3174f6b6a9d0e5c
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2021-12-30 00:38:24 +0530

    challenge 1
     click event --> again button

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,19 +10,28 @@
 
 // adding a click event
 
-const number = Math.trunc(Math.random()*20)+1;
+let number = Math.trunc(Math.random()*20)+1;
 let score = 20;
-document.querySelector('.number').textContent = number;
+
 
 
 document.querySelector('.check').addEventListener('click' , function () {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess);
 
+
+    // no number
     if (!guess) {
         document.querySelector('.message').textContent = 'No number entererd';
+
+    // wright number
     }else if (guess===number) {
         document.querySelector('.message').textContent = 'correct number';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        document.querySelector('.number').textContent = number;
+
+    // number too high
     }else if (guess>number) {
 
             if(score>1){
@@ -34,6 +43,8 @@ document.querySelector('.check').addEventListener('click' , function () {
                 document.querySelector('.message').textContent = 'you lost the game';
                 document.querySelector('.score').textContent = 0;
             }
+
+    // number too low
     }else if (guess<number) {
 
         if(score>1){
@@ -48,3 +59,18 @@ document.querySelector('.check').addEventListener('click' , function () {
         
     }
 });
+
+// challange 1   --> to restore the page using again button;
+
+document.querySelector('.again').addEventListener('click', function(){
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    number = Math.trunc(Math.random()*20)+1;
+    document.querySelector('.number').textContent = '?';
+
+    // restoring views
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+})
