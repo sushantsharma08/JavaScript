a5167eba41fcbcefbc7406d0b2a9214ec058cd01
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2021-12-29 11:27:34 +0530

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,2 +1,8 @@
 'use strict';
  console.log(document.querySelector('.message').textContent);
+
+ document.querySelector('.message').textContent = 'yoo boi wassup';
+
+ document.querySelector('.number').textContent = 4;
+ document.querySelector('.score').textContent = 30;
+ document.querySelector('.guess').value = 7;
