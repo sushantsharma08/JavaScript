e56483b4609c0cbc790685e8e20b96a676abc399
Sushant Sharma <88335357+sushantsharma08@users.noreply.github.com>
2022-01-03 23:48:32 +0530

    switching player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -10,8 +12,9 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+const score = [0,0];
 let currrentScore = 0;
-
+let activePlayer = 0;
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
@@ -29,8 +32,14 @@ btnRoll.addEventListener('click', function(){
         if(dice !== 1){
             // Add dice to current score
             currrentScore += dice;
-            current0El.textContent = currrentScore;
+            document.getElementById(`current--${activePlayer}`).textContent = currrentScore;
+            // current0El.textContent = currrentScore;
         }else{
             //switch player
+            document.getElementById(`current--${activePlayer}`).textContent=0;
+            currrentScore=0;
+            activePlayer = activePlayer === 0 ? 1 : 0;
+            player0El.classList.toggle('player--active');
+            player1El.classList.toggle('player--active');
         }
 });
