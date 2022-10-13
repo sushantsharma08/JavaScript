6261add0dd383c2edf44ef6595995ab0141b5162
Sushant Sharma <ssushantlb08@gmail.com>
2022-10-13 23:40:17 +0530

    Update script.js

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -237,61 +237,63 @@
 //   console.log(`on ${key} we open at ${open} and close at ${close}` );
 // }
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
-    'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
 
-const [players1,players2] = game.players
-console.log(players1, players2 );
-const [gk1,...feildPlayers1]=players1
-console.log(`Team 1 : 
-goalkeeper : ${gk1},
-feildPlayers : ${feildPlayers1}`);
-const [gk2,...feildPlayers2]=players2
-console.log(`Team 2 : 
-goalkeeper : ${gk2},
-feildPlayers : ${feildPlayers2}`);
-const allPlayers = [...players1,...players2]
-console.log(allPlayers)
-const players1Final=[...players1,'thiago','Coutinho','perisic']
-console.log(players1Final);
-const {team1,draw,team2}=game.odds
-console.log(team1,draw,team2);
+// _____________________challange1_________________________________
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+//     'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// const [players1,players2] = game.players
+// console.log(players1, players2 );
+// const [gk1,...feildPlayers1]=players1
+// console.log(`Team 1 : 
+// goalkeeper : ${gk1},
+// feildPlayers : ${feildPlayers1}`);
+// const [gk2,...feildPlayers2]=players2
+// console.log(`Team 2 : 
+// goalkeeper : ${gk2},
+// feildPlayers : ${feildPlayers2}`);
+// const allPlayers = [...players1,...players2]
+// console.log(allPlayers)
+// const players1Final=[...players1,'thiago','Coutinho','perisic']
+// console.log(players1Final);
+// const {team1,x:draw,team2}=game.odds
+// console.log(team1,draw,team2);
