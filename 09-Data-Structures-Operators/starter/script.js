6bcefd50df077c74b87057c9ab3f69af5def045c
Sushant Sharma <ssushantlb08@gmail.com>
2023-02-11 21:09:43 +0530

    Check for occurrence of alphabets in a String

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -373,3 +373,18 @@ console.log(rest.get(time >= rest.get('open') && time <= rest.get('closed')));
 
 console.log(rest.has('name'));
 rest.delete(2);
+console.log(rest);
+
+console.log("_____________________Check for occurances of alphabets in a String_____________________");
+
+const allAlpha = {
+  a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0, q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0, " ":0
+}
+
+const strToCheck = "sushant sharma";
+
+for (let i = 0; i < strToCheck.length; i++) {
+  allAlpha[strToCheck[i]]+=1
+}
+
+console.log(allAlpha);
