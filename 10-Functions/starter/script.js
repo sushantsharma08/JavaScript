a373103fdcd49c899e4c7576de76ea1b954ce313
Sushant Sharma <ssushantlb08@gmail.com>
2023-02-17 01:05:07 +0530

    default parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const bookings=[];
+
+const createBooking=function(flightNum,numPassengers=1,price=1000*numPassengers){
+    
+    const booking={
+        flightNum,
+        numPassengers,
+        price
+    }
+    console.log(booking);
+    bookings.push(booking);
+
+}
+createBooking(423,8);
+createBooking(873,2);
+createBooking(911);
+createBooking(461,9);
+console.log(bookings);
